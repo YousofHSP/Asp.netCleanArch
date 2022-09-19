668a279ae75337fe05fdd7bc7e872d69700ba1df
Yousof Hosseinpour <yousof.hosseinpour@gmail.com>
2022-09-19 18:40:02 +0430

    Course Repository

--- CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch.Application/Interfaces/ICourseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArch.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace CleanArch.Application.Interfaces
 {
-    internal interface ICourseService
+    public interface ICourseService
     {
+        CourseViewModel GetCourses();
     }
 }
